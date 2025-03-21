a1868f8b0834ee4907c7c361619e0fdbd707a3d4
rizki.audivimagi <rizki.audivimagi@gkspay.com>
2025-03-22 04:25:09 +0700

    Create menu antrian pelanggan

--- Main.java
+++ Main.java
@@ -1,11 +1,47 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        Antrian queue = new Antrian();
-        queue.push("Pelanggan 1");
-        queue.push("Pelanggan 2");
-        queue.push("Pelanggan 3");
-        queue.pop();
-        queue.pop();
-        queue.pop();
+        Scanner scanner = new Scanner(System.in);
+        Antrian antrian = new Antrian();
+
+        boolean exit = false;
+        while (!exit) {
+            System.out.println("Selamat Datang di Sistem Antrian Pelanggan Rumah Makan!");
+            boolean userExit = false;
+            while (!userExit) {
+                System.out.println("=======================================================");
+                System.out.println("Pilih tindakan :");
+                System.out.println("1. Tambah Antrian Pelanggan (Push)");
+                System.out.println("2. Lepas antrian pelanggan terdepan (Pop)");
+                System.out.println("3. Tampilkan antrian");
+                System.out.println("4. Exit");
+
+                int action = scanner.nextInt();
+                scanner.nextLine();
+
+                switch (action) {
+                    case 1 :
+                        System.out.println("Masukkan nama pelanggan :");
+                        String name = scanner.nextLine();
+                        antrian.push(name);
+                        break;
+                    case 2 :
+                        antrian.pop();
+                        break;
+                    case 3 :
+                        antrian.displayQueue();
+                        break;
+                    case 4 :
+                        userExit = true;
+                        exit = true;
+                        break;
+                    default :
+                        System.out.println("Pilihan tidak valid.");
+                }
+            }
+
+        }
+        scanner.close();
     }
 }
